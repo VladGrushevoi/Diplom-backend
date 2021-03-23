9d6c8913c628149f41a052e83cfba418f14b9621
Vladislav Hrushovyi <vlad.grush.99@gmail.com>
2021-03-23 16:32:08 +0200

    Changes

--- Startup.cs
+++ Startup.cs
@@ -1,15 +1,12 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Services;
+using Services.AdminRepositories;
+using UseCase.Admin;
 
 namespace DiplomBackend
 {
@@ -26,6 +23,10 @@ namespace DiplomBackend
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddDbContext<DbAppContext>(options => 
+            options.UseNpgsql(Configuration.GetConnectionString("DiplomDatabase")));
+            services.AddScoped<IAdminRepository, AdminRepository>();
+            services.AddScoped<UpdateAppartUseCase, UpdateAppartUseCase>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
