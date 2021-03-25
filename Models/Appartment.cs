0510630c7fd571c73d95914bd7b89209dac49800
Vladislav Hrushovyi <vlad.grush.99@gmail.com>
2021-03-25 23:22:33 +0200

    Add attribute

--- Models/Appartment.cs
+++ Models/Appartment.cs
@@ -1,12 +1,19 @@
+using Microsoft.ML.Data;
+
 namespace Models
 {
     public class Appartment : BaseMosel
     {
-        public double TotalSquare { get; set; }
-        public int RoomsCount { get; set; }
-        public string StreetName { get; set; }
-        public int Floor { get; set; }
-        public double Price { get; set; }
+        [ColumnName("TotalSquare")]
+        public float TotalSquare { get; set; }
+        [ColumnName("RoomsCount")]
+        public float RoomsCount { get; set; }
+        [ColumnName("DistrictValue")]
+        public float DistrictValue { get; set; }
+        [ColumnName("Floor")]
+        public float Floor { get; set; }
+        [ColumnName("Price")]
+        public float Price { get; set; }
 
         public Appartment(){}
     }
