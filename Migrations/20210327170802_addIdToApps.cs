11ce288d9b646bdc3aaa93577bd57c98020c6474
Vladislav Hrushovyi <vlad.grush.99@gmail.com>
2021-03-28 14:32:51 +0300

    Add new migration

---
+++ Migrations/20210327170802_addIdToApps.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DiplomBackend.Migrations
+{
+    public partial class addIdToApps : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "IdFromApi",
+                table: "apartment",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IdFromApi",
+                table: "apartment");
+        }
+    }
+}
