6ef50e25a039bb6f9a5bad3073f39f9fba7a0363
Vladislav Hrushovyi <vlad.grush.99@gmail.com>
2021-04-22 17:34:42 +0300

    add new table

--- Services/DbAppContext.cs
+++ Services/DbAppContext.cs
@@ -6,6 +6,10 @@ namespace Services
     public class DbAppContext : DbContext
     {
         public DbSet<Appartment> apartment { get; set; }
+        public DbSet<User> user { get; set; }
+        public DbSet<District> districts { get; set; }
+        public DbSet<TypePlace> typePlaces { get; set; }
+        public DbSet<ImportantPlace> importantPlaces { get; set; }
         public DbAppContext(DbContextOptions opt) : base(opt)
         {
 
