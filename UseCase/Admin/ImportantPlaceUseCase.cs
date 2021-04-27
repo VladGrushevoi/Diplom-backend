13171e30c4658be17d22dd5fea8b980196f21f37
Vladislav Hrushovyi <vlad.grush.99@gmail.com>
2021-04-27 14:04:32 +0300

    add method

--- UseCase/Admin/ImportantPlaceUseCase.cs
+++ UseCase/Admin/ImportantPlaceUseCase.cs
@@ -24,5 +24,23 @@ namespace UseCase.Admin
             var place = adminRepository.DeletePlace(id).Result;
             return new JsonResult(new { Message = "Місце видалено", Item = place });
         }
+
+        public IActionResult GetAllPlaces()
+        {
+            var places =  adminRepository.GetAllPlace().Result;
+            return new JsonResult(new { places = places });
+        }
+
+        public IActionResult GetPlaceByDistrictName(string name)
+        {
+            var places = adminRepository.GetPlaceByDistrictName(name).Result;
+            return new JsonResult(new { items = places });
+        }
+
+        public IActionResult GetOlaceById(int id)
+        {
+            var place = adminRepository.GetPlaceById(id).Result;
+            return new JsonResult( new { place = place } );
+        }
     }
 }
