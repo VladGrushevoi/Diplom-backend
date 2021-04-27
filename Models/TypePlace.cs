64e77e686a15aef41afb7641e671331d7fe28db4
Vladislav Hrushovyi <vlad.grush.99@gmail.com>
2021-04-27 14:03:19 +0300

    add field

--- Models/TypePlace.cs
+++ Models/TypePlace.cs
@@ -5,6 +5,7 @@ namespace Models
     public class TypePlace : BaseMosel
     {
         public string Name { get; set; }
+        public int Count { get; set; }
 
         public List<ImportantPlace> ImportantPlace { get; set; }
     }
