7e8e05be8f09dcbd8f64e761c68ba672c1a69f19
Vladislav Hrushovyi <vlad.grush.99@gmail.com>
2021-04-27 14:03:12 +0300

    change field type

--- Models/ImportantPlace.cs
+++ Models/ImportantPlace.cs
@@ -2,8 +2,8 @@ namespace Models
 {
     public class ImportantPlace : BaseMosel
     {
-        public int DistrictId { get; set; }
-        public int TypePlaceId { get; set; }
+        public string DistrictName { get; set; }
+        public string TypePlaceName { get; set; }
         public double Longitude { get; set; }
         public double Latitude { get; set; }
 
